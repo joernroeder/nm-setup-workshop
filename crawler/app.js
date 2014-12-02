689ff96ed16403669423772ba2791cf1511d52cc
Jörn Röder <kontakt@joernroeder.de>
2014-12-02 12:04:57 +0100

    cleanup

--- crawler/app.js
+++
@@ -1,8 +0,0 @@
-var Pool = require('./Pool.js');
-var Crawler = require('./Crawler');
-
-var pool = new Pool(10, Crawler);
-
-pool.addToQueue('http://spiegel.de', function () {
-	pool.startCrawler();
-});
